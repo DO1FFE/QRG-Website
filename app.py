ff073a6c522a8c1d45e3013c3ba681a0656e4014
Erik Schauer <do1ffe@darc.de>
2023-06-30 16:44:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,56 @@
-import datetime
-import time
+from flask import Flask, request, render_template, make_response, redirect, url_for, jsonify
+import json
 import threading
-import requests
-from bs4 import BeautifulSoup
-from flask import Flask, request, render_template, make_response, jsonify
+import datetime
+from datetime import timedelta
+import time
+from requests import get
 
 app = Flask(__name__)
-stations = {}
-
-def validate_callsign(callsign):
-    url = f"https://ans.bundesnetzagentur.de/Amateurfunk/Rufzeichen.aspx"
-    payload = {'Rufzeichen': callsign}
-    response = requests.get(url, params=payload)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    return bool(soup.find('span', text=callsign))
+db = {}
 
 def cleanup_thread():
     while True:
-        current_time = datetime.datetime.utcnow()
-        keys_to_delete = [key for key, value in stations.items() if (current_time - value['timestamp']).total_seconds() > 24*60*60]
-        for key in keys_to_delete:
-            del stations[key]
+        now = datetime.datetime.utcnow()
+        to_delete = [callsign for callsign, data in db.items() if now - data['timestamp'] > timedelta(hours=24)]
+        for callsign in to_delete:
+            del db[callsign]
         time.sleep(60)
+
+cleanup_thread = threading.Thread(target=cleanup_thread)
+cleanup_thread.start()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        callsign = request.form.get('callsign', '')
-        frequency = request.form.get('frequency', '')
-        mode = request.form.get('mode', '')
-        if callsign and frequency and mode and validate_callsign(callsign):
-            stations[callsign] = {
-                'frequency': round(float(frequency), 4),
-                'mode': mode,
-                'timestamp': datetime.datetime.utcnow()
-            }
-        resp = make_response(render_template('index.html', stations=stations))
+        callsign = request.form.get('callsign')
+        frequency = round(float(request.form.get('frequency')), 4)
+        mode = request.form.get('mode')
+
+        resp = make_response(redirect(url_for('index')))
+
+        db[callsign] = {
+            'callsign': callsign,
+            'frequency': frequency,
+            'mode': mode,
+            'timestamp': datetime.datetime.utcnow(),
+        }
+
         resp.set_cookie('callsign', callsign)
         resp.set_cookie('frequency', str(frequency))
         resp.set_cookie('mode', mode)
+
         return resp
-    else:
-        callsign = request.cookies.get('callsign', '')
-        frequency = request.cookies.get('frequency', '')
-        mode = request.cookies.get('mode', '')
-        return render_template('index.html', stations=stations, callsign=callsign, frequency=frequency, mode=mode)
+
+    callsign = request.cookies.get('callsign', '')
+    frequency = request.cookies.get('frequency', '')
+    mode = request.cookies.get('mode', '')
+
+    return render_template('index.html', callsign=callsign, frequency=frequency, mode=mode)
 
 @app.route('/data', methods=['GET'])
 def data():
-    return jsonify(stations)
+    return jsonify(db)
 
 if __name__ == '__main__':
-    cleanup = threading.Thread(target=cleanup_thread)
-    cleanup.start()
-    app.run(host='0.0.0.0', port=8080)
+    app.run(port=8080, debug=True)

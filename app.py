a83886de96e8592a4959a91a291a73017e256108
Erik Schauer <do1ffe@darc.de>
2023-06-30 17:33:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,71 +1,58 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response
-import shelve
-import datetime
-import threading
+from flask import Flask, request, render_template, make_response
 import time
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
-db = shelve.open('database.db', writeback=True)
 
-def cleanup_thread():
-    while True:
-        now = datetime.datetime.utcnow()
+def create_connection():
+    conn = None;
+    try:
+        conn = sqlite3.connect('database.db')       # Name der SQLite-Datenbank-Datei
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    return conn
 
-        for key in list(db.keys()):
-            if (now - db[key]['timestamp']).total_seconds() > 600:
-                del db[key]
+def create_table(conn):
+    try:
+        sql_create_table = """ CREATE TABLE IF NOT EXISTS qso (
+                                            id integer PRIMARY KEY AUTOINCREMENT,
+                                            time text NOT NULL,
+                                            callsign text NOT NULL,
+                                            frequency real NOT NULL
+                                        ); """
+        if conn is not None:
+            c = conn.cursor()
+            c.execute(sql_create_table)
+    except Error as e:
+        print(e)
 
-        time.sleep(60)
-
-threading.Thread(target=cleanup_thread, daemon=True).start()
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    conn = create_connection()
+    create_table(conn)
     if request.method == 'POST':
-        callsign = request.form.get('callsign', '')
-        frequency = request.form.get('frequency')
-        mode = request.form.get('mode')
-
-        if frequency:
-            frequency = frequency.replace(',', '.')
-            frequency = round(float(frequency), 4)
-        else:
-            frequency = 0
-
-        resp = make_response(redirect(url_for('index')))
-
-        db[callsign] = {
-            'callsign': callsign,
-            'frequency': frequency,
-            'mode': mode,
-            'timestamp': datetime.datetime.utcnow(),
-        }
-
+        callsign = request.form.get('callsign')
+        frequency = round(float(request.form.get('frequency').replace(",", ".")), 4)
+        cur = conn.cursor()
+        cur.execute("INSERT INTO qso (time, callsign, frequency) VALUES (?, ?, ?)",
+                    (time.strftime("%Y-%m-%d %H:%M:%S"), callsign, frequency))
+        conn.commit()
+        resp = make_response(render_template('index.html'))
         resp.set_cookie('callsign', callsign)
-        resp.set_cookie('frequency', str(frequency))
-        resp.set_cookie('mode', mode)
-
         return resp
+    else:
+        callsign = request.cookies.get('callsign')
+        return render_template('index.html', callsign=callsign)
 
-    callsign = request.cookies.get('callsign', '')
-    frequency = request.cookies.get('frequency', '')
-    mode = request.cookies.get('mode', '')
-
-    return render_template('index.html', callsign=callsign, frequency=frequency, mode=mode)
-
-@app.route('/data')
+@app.route("/data", methods=['GET'])
 def data():
-    return {
-        'data': [
-            {
-                'callsign': entry['callsign'],
-                'frequency': entry['frequency'],
-                'mode': entry['mode'],
-                'timestamp': entry['timestamp'].isoformat() + 'Z',
-            }
-            for entry in db.values()
-        ]
-    }
+    conn = create_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM qso ORDER BY time DESC LIMIT 10")
+    rows = cur.fetchall()
+    return render_template('data.html', rows=rows)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
